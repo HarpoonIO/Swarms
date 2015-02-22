83d9b39a59b62b92747a651cc229420dae4ad45c
ThomasHedegaard <thomashj93@gmail.com>
2015-02-22 13:25:21 +0100

    First test is green, and all dependencies are up and running

---
+++ tests/test_bee.js
@@ -0,0 +1,10 @@
+describe('Factories in project FifthElement', function () {
+
+  it("should be green!", function(){
+      console.log("------------> First test");
+
+      expect(2).toEqual(2);
+
+  });
+
+});
